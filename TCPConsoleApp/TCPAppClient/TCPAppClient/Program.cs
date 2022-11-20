90c06dfbc672bf03e37dadc9fb9e8ea47b4bfaaa
NonThanadol <81633477+nonthanadol@users.noreply.github.com>
2022-11-20 15:45:33 +0700

    Add files via upload

---
+++ TCPConsoleApp/TCPAppClient/TCPAppClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+
+
+namespace TCPIPAppClient
+{
+    class ClientApp
+    {
+        static void Main(string[] args)
+        {
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // Create object -> IPv4,TCP
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"),1990);
+            sck.Connect(endpoint); // Connect server
+            Console.Write("Enter massage : ");
+            string msg = Console.ReadLine();
+            byte[] msgbuffer = Encoding.ASCII.GetBytes(msg);
+            
+            sck.Send(msgbuffer,0,msgbuffer.Length,0);
+            Console.WriteLine("Send data complete");
+
+            byte[] buffer = new byte[1024];
+            int rec = sck.Receive(buffer,0,buffer.Length,0);
+            Array.Resize(ref buffer, rec);
+            Console.WriteLine("Recive encoding buffer : {0} " ,Encoding.Default.GetString(buffer));
+            sck.Close();
+            Console.Read();
+
+        }
+
+    }
+
+}
---
+++ TCPConsoleApp/TCPIPAppBasic/TCPIPAppBasic/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+
+
+namespace TCPIPAppBasic
+{
+    class ServerApp 
+    {
+        static void Main(string[] args) 
+        {
+            Socket sck = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp); // Create object -> IPv4,TCP
+
+            sck.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"),1990));// open port , bind socket with IP
+            Console.WriteLine("Wait for connect");
+            sck.Listen(10); //This socket support connection 10 machine 
+
+            Socket acc = sck.Accept(); // connected success
+            Console.WriteLine("Connect complete");
+
+            //.....Send data to client
+            string message = "Hello";
+            byte[] buffer = Encoding.ASCII.GetBytes(message); // Encoding data ASCII to Bytes
+            
+            acc.Send(buffer,0,buffer.Length,0); // Send data to client MC
+            Console.WriteLine("Send data complete");
+
+            //.....Recive from client
+            buffer = new byte[1024]; // reserve array 
+            int rec = acc.Receive(buffer, 0, buffer.Length, 0); // Recive element data 
+            
+            Array.Resize(ref buffer, rec); // Change diamention array "buffer" to same size "rec" 
+            Console.WriteLine("Recived encoding buffer : {0}", Encoding.Default.GetString(buffer));
+            Console.WriteLine("Recived  buffer : {0}", buffer);
+            Console.WriteLine("Recived  rec : {0}",rec);
+           
+            sck.Close(); // Close Port
+            acc.Close();
+            Console.Read();
+        
+        }
+
+    }
+
+}
