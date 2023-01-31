aa283741ea791080b07dd2840f1af6e74cf5d6b3
NonThanadol <81633477+nonthanadol@users.noreply.github.com>
2023-01-31 21:27:45 +0700

    Add files via upload

---
+++ Basic_ModbusTCP_DeltaPLC1.10/Basic_ModbusTCP_DeltaPLC/UserControl3.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Basic_ModbusTCP_DeltaPLC
+{
+    public partial class UserControl3 : UserControl
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-G7CTSGR\TEW_SQLEXPRESS;Initial Catalog=demo;Integrated Security=True");
+        public UserControl3()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl3_Load(object sender, EventArgs e)
+        {
+            disp_data();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into tblCountScrew values('"+textBox1.Text+"','"+ textBox2.Text + "','"+ textBox3.Text + "','"+ textBox4.Text + "','"+ textBox5.Text + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            disp_data();
+            MessageBox.Show("Record inserted successfully");
+        }
+
+        public void disp_data()
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from tblCountScrew";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from tblCountScrew where Model='"+textBox1.Text+"'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            textBox1.Text = "";
+            disp_data();
+            MessageBox.Show("Record inserted successfully");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update tblCountScrew set Model='"+textBox6.Text+ "' where Model='" + textBox1.Text + "' ";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            textBox6.Text = "";
+            disp_data();
+            MessageBox.Show("Record update successfully");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            disp_data();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from tblCountScrew where Model='"+textBox1.Text+"'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+    }
+}
